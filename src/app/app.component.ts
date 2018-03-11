0b0d52d43ec8733a6ce2d14b09520a5b47a0cf7c
l08084 <l08084@shibaura-it.ac.jp>
2018-03-11 18:38:37 +0900

    ドキュメンテーションコメント追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,6 +51,75 @@ export class AppComponent implements AfterViewInit {
   }
 
   /**
+   * 初期化
+   *
+   * @memberof AppComponent
+   */
+  init = (): void => {
+    const maxY: number
+      = window.innerHeight - this.bird.nativeElement.height;
+    this.action.setMaxY(maxY);
+    this.action.setAy(0.4);
+    this.action.setVy(0);
+    this.action.setY(0);
+    this.start();
+  }
+
+  /**
+   * ゲームスタート
+   * 各処理のインターバルを設定する
+   *
+   * @memberof AppComponent
+   */
+  start = (): void => {
+    // 20msごとにbirdの落下処理と衝突判定を呼び出す
+    this.subscription = Observable.interval(20)
+      .subscribe(() => {
+        this.action.moveBird();
+        this.checkCollision();
+      });
+
+    // 2sごとに壁の設置処理を呼び出す
+    this.wallSubscription = Observable.interval(2000)
+      .subscribe(() => this.setWall());
+
+    // 20msごとに壁の移動処理を呼び出す
+    this.moveWallSubscription = Observable.interval(20)
+      .subscribe(() => this.moveWall());
+
+    // 20msごとに背景の移動処理を呼び出す
+    this.moveBackgroundSubscription = Observable.interval(20)
+      .subscribe(() => this.action.moveBackground());
+
+    // 1sごとにスコアのカウントを行う
+    this.scoreSubscription = Observable.interval(1000)
+      .subscribe(() => this.action.scoreIncrement());
+  }
+
+  /**
+   * 各処理のインターバルを停止する
+   *
+   * @memberof AppComponent
+   */
+  end = (): void => {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    if (this.wallSubscription) {
+      this.wallSubscription.unsubscribe();
+    }
+    if (this.moveWallSubscription) {
+      this.moveWallSubscription.unsubscribe();
+    }
+    if (this.moveBackgroundSubscription) {
+      this.moveBackgroundSubscription.unsubscribe();
+    }
+    if (this.scoreSubscription) {
+      this.scoreSubscription.unsubscribe();
+    }
+  }
+
+  /**
    * 壁を設置する
    *
    * @memberof AppComponent
@@ -72,10 +141,16 @@ export class AppComponent implements AfterViewInit {
     this.renderer2.setStyle(wallTop, 'left', `${maxX}px`);
     this.renderer2.setStyle(wallBottom, 'left', `${maxX}px`);
 
+    // 壁(div)を設置する
     this.renderer2.appendChild(this.el.nativeElement, wallTop);
     this.renderer2.appendChild(this.el.nativeElement, wallBottom);
   }
 
+  /**
+   * 設置した壁を横に動かす
+   *
+   * @memberof AppComponent
+   */
   moveWall = (): void => {
     const wallList = document.getElementsByClassName('wall') as HTMLCollectionOf<HTMLElement>;
     for (let i = 0; i < wallList.length; i++) {
@@ -90,16 +165,11 @@ export class AppComponent implements AfterViewInit {
     }
   }
 
-  init = (): void => {
-    const maxY: number
-      = window.innerHeight - this.bird.nativeElement.height;
-    this.action.setMaxY(maxY);
-    this.action.setAy(0.4);
-    this.action.setVy(0);
-    this.action.setY(0);
-    this.start();
-  }
-
+  /**
+   * 衝突判定
+   *
+   * @memberof AppComponent
+   */
   checkCollision = () => {
     const wallList = document.getElementsByClassName('wall');
     const birdRect = this.bird.nativeElement.getBoundingClientRect();
@@ -110,40 +180,6 @@ export class AppComponent implements AfterViewInit {
           this.action.gameOver();
         }
       }
-    }
-  }
-
-  start = (): void => {
-    this.subscription = Observable.interval(20)
-      .subscribe(() => {
-        this.action.moveBird();
-        this.checkCollision();
-      });
-    this.wallSubscription = Observable.interval(2000)
-      .subscribe(() => this.setWall());
-    this.moveWallSubscription = Observable.interval(20)
-      .subscribe(() => this.moveWall());
-    this.moveBackgroundSubscription = Observable.interval(20)
-      .subscribe(() => this.action.moveBackground());
-    this.scoreSubscription = Observable.interval(1000)
-      .subscribe(() => this.action.scoreIncrement());
-  }
-
-  end = (): void => {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-    if (this.wallSubscription) {
-      this.wallSubscription.unsubscribe();
-    }
-    if (this.moveWallSubscription) {
-      this.moveWallSubscription.unsubscribe();
-    }
-    if (this.moveBackgroundSubscription) {
-      this.moveBackgroundSubscription.unsubscribe();
-    }
-    if (this.scoreSubscription) {
-      this.scoreSubscription.unsubscribe();
     }
   }
 
